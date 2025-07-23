614a90b07adf67900df0331594f8f48603732377
Soldrion <edinmemes@gmail.com>
2025-07-23 01:49:53 -0400

    change rebuild script

--- tracking ap/thing_tracker/rebuild.sh
+++ tracking ap/thing_tracker/rebuild.sh
@@ -3,7 +3,15 @@ set -e
 echo "Starting rebuild..."
 echo "*********************************"\n
 echo "*********************************"\n
-echo "Make sure you have no .py, .bak, or .spec files in the directory where this script is run."
+echo "Make sure you have no other .py, .bak, .txt, .md, or .spec files in the directory where this script is run."
+read -r -p "Are you sure? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    continue
+else
+    echo "Please close this window."
+fi
+
 echo "*********************************"\n
 echo "*********************************"\n
 cd "$(dirname "$0")"
@@ -11,13 +19,16 @@ cd "$(dirname "$0")"
 if [[ -d venv311 ]]; then
     source venv311/bin/activate
 fi
-
+pyinstaller --onefile updater.py
 pyinstaller --onefile --add-data "ui_main.py:." --add-data "storage.py:." --add-data "notifier.py:." --add-data "tracker_model.py:." --add-data "utils.py:." --add-data "version.txt:." main.py
 echo "*********************************"
 echo "Rebuild complete. Cleaning up..."
 echo "*********************************"
-rm -f *.py *.bak *.spec
+rm -f *.py *.bak *.spec *.txt *.md
 rm -rf build
 mv dist/main $("pwd")/main
+mv dist/updater $("pwd")/updater
+rm -rf dist
 
-exit
+echo "Please close this window."
+done
